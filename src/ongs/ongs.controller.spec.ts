fe5a337fd381b363dea5f72516809f5065121ba0
Rafael Scotti Zanella <rafaels.zanella@gmail.com>
2021-12-04 21:54:13 -0300

    test(ongs.controller): add unit tests

--- src/ongs/ongs.controller.spec.ts
+++ src/ongs/ongs.controller.spec.ts
@@ -1,20 +1,49 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { OngsController } from './ongs.controller';
 import { OngsService } from './ongs.service';
 
 describe('OngsController', () => {
-  let controller: OngsController;
+  let ongsController: OngsController;
+  let ongsService: OngsService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [OngsController],
-      providers: [OngsService],
-    }).compile();
+    ongsService = new OngsService();
+    ongsService.findAll = jest.fn();
+    ongsService.findOne = jest.fn();
+    ongsService.create = jest.fn();
+    ongsService.update = jest.fn();
+    ongsService.remove = jest.fn();
 
-    controller = module.get<OngsController>(OngsController);
+    ongsController = new OngsController(ongsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('deve chamar service findAll', () => {
+    ongsController.findAll();
+    expect(ongsService.findAll).toHaveBeenCalled();
+  });
+
+  it('deve chamar service findOne', () => {
+    ongsController.findOne('123456');
+    expect(ongsService.findOne).toHaveBeenCalledWith('123456');
+  });
+
+  it('deve chamar service create()', () => {
+    const mock = {
+      nome: 'teste',
+      cnpj: '123456',
+      telefone: '33332222',
+    };
+
+    ongsController.create(mock);
+    expect(ongsService.create).toHaveBeenCalledWith(mock);
+  });
+
+  it('deve chamar service update()', () => {
+    const mock = {
+      nome: 'teste1',
+      telefone: '33332222',
+    };
+
+    ongsController.update('123456', mock);
+    expect(ongsService.update).toHaveBeenCalledWith('123456', mock);
   });
 });
